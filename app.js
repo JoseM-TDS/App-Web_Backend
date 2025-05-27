14bfcaba62104bdf0ba38b5fcccd7d1f9b6c745f
Jose Melendez <23001327@galileo.edu>
2025-05-26 23:12:11 -0600

    Feat: conexion a mysql

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mysql = require('mysql');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,6 +21,52 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+// Connection with Mysql
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'mysql'
+});
+
+connection.connect(function(err) {
+  if(err){
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  
+  console.log('Connected as id: ' + connection.threadId);
+});
+
+let queryCreateDB = 'CREATE DATABSE IF NOT EXISTS webApplication;';
+let queryCreateTableGoals = 'CREATE TABLE IF NOT EXISTS `goals` ( \
+`id` int(12) NOT NULL auto_increment, \
+`name` varchar(250) NOT NULL default \'\', \
+`description` varchar(250) NOT NULL default \'\', \
+`date` varchar(250) NOT NULL default \'\', \
+PRIMARY KEY (`id`) \
+);'
+
+connection.query(queryCreateDB, function(err, results, fields) {
+  if(err){
+    console.error('Failed to create DB' + err);
+    return;
+  }else{
+    console.error('Results: ' + results);
+    console.error('Fields: ' + fields);
+  }
+});
+
+connection.query(queryCreateTableGoals, function(err, results, fields) {
+  if(err){
+    console.error('Failde to create Table' + err);
+    return;
+  }else{
+    onsole.error('Results: ' + results);
+    console.error('Fields: ' + fields);
+  }
+});
+connection.destroy();
 
 // Middleware
 app.use((req, res, next) => {
