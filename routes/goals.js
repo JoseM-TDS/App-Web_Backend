492a0109bdaa83e1d5ceda9a67b29e3f95b57914
Jose Melendez <23001327@galileo.edu>
2025-05-26 23:29:45 -0600

    Implementacion mysql en Goals

--- routes/goals.js
+++ routes/goals.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+var mysql = require('mysql');
 
 let goals = [
     {
@@ -19,29 +20,66 @@ let goals = [
     }
 ]
 
+// Connection with mysql
+var connection = mysql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  database: 'mysql'
+});
+
+connection.connect(function(err) {
+  if(err){
+    console.error('Error connecting: ' + err.stack);
+    return;
+  }
+  
+  console.log('Connected as id: ' + connection.threadId);
+});
+
 router.get('/getGoals', function(req, res, next) {
-    res.json(goals);
+    var queryGetGoal = 'SELECT * FROM goals';
+    connection.query(queryGetGoal, function(err, results) {
+        if(err){
+            console.error('Failed to create goal' + err);
+            res.status(500).json(err);
+        }else{
+            res.json(results);
+        }
+    });
 });
 
 router.delete('/deleteGoal/:id', function(req, res, next) {
-    const goalId = parseInt(req.params.id);
-    const goal = goals.find(goal => goal.id !== goalId);
-    if(!goal) {
-        return res.status(400).json({ message: 'Goal not found'});
-    } else {
-        goals = goals.filter(goal => goal.id !== goalId);
-        res.status(200).json({ message: 'Goal deleted successfully'});
+    if(req.params && req.params.id){
+        let id = req.params.id;
+        let queryDeleteGoal = 'DELETE FROM goals WHERE id="' + id + '"';
+        connection.query(queryDeleteGoal, function(err, results) {
+            if(err){
+                console.error('Failed to delete goal' + err);
+                res.status(500).json(err);
+            }else{
+                res.status(200).json(results);
+            }
+        });
+    }else { 
+        res.status(400).json({});
     }
 });
 
 router.post('/addGoal', function(req, res, next) {
-    const newGoal = {
-        id: goals.length + 1,
-        name: req.body.name,
-        description: req.body.description
-    };
-    goals.push(newGoal);
-    req.status(200).json({ message: 'Goal added successfully' });
+    if(req.body && req.body.name && req.body.description && req.body.date){
+        let queryAddGoal = 'INSERT INTO goals (name, description, date \
+        VALUES ("' + req.body.name + '", "' + req.body.description + '", "' + req.body.date + '");';
+        connection.query(queryDeleteGoal, function(err, results) {
+            if(err){
+                console.error('Failed to add goal' + err);
+                res.status(400).json(err);
+            }else{
+                res.status(200).json(results);
+            }
+        });
+    }else { 
+        res.status(400).json({});
+    }
 });
 
 module.exports = router;
